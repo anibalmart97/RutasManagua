03e54ec0e9edaccb169e498ae1a7aec9ca63fe2f
Anibal A Martinez <anibalmart97@gmail.com>
2017-05-15 06:40:13 -0600

    Actualizacion Rutas de Managua V 1.5

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,19 @@
 class HomeController < ApplicationController
+
+  ###Lista los tres primeros articulos en caso de que existan
   def index
+    @Articles=Article.all
+    @Articulos=[]
+    for line in (0...3)
+      if  @Articles[line] then
+        @Articulos << @Articles[line]
+      end
+    end
   end
-end
+
+  ###Permite la Visualizacion de un Articulo en Especifico
+  def show
+    @Articulo = Article.find(params[:id])
+  end
+
+end ##Fin de Controlador
